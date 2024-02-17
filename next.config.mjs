893779d2f8c5b3f56457ca46401f42dc27122519
Jacob Lantsman <jacob.lantsman@gmail.com>
2024-02-16 21:30:50 -0500

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
+import { hostname } from 'os';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'liveblocks.io',
+                port: ''
+            }
+        ]
+    },
+    webpack: (
+        config,
+        { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+    ) => {
+        config.externals.push({ canvas: 'commonjs canvas' })
+        return config
+    },
+};
 
 export default nextConfig;
